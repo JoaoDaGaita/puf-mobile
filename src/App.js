88c38413fc1e8091539967d859fa73f506d2a94c
João Vicente <joaovsilva70@outlook.com>
2022-12-15 17:12:06 -0300

    feat: setup react navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
 import * as React from "react";
-import { Theme } from "~/components/Theme";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { Theme } from "~/components/Theme";
 import { App } from "./pages";
+
+const Stack = createNativeStackNavigator();
 
 export const Main = () => {
   return (
     <Theme>
-      <App />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="/login"
+            component={App}
+            options={{
+              headerTitleAlign: "center",
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Theme>
   );
 };
